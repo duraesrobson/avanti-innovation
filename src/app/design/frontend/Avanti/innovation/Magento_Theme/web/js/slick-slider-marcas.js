86c9f6978bac844f1510a360bdd1b40c674ff5bf
Robson Durães <duraesrobson@gmail.com>
2025-12-19 14:57:39 -0300

    feat(main-content): carrossel de marcas com slick apenas no desktop
    - ativa slick somente a partir de 768px
    - remove slick no mobile
    - ajusta layout mobile para grid com flex-wrap
    - corrige espaçamentos e exibição das logos

--- src/app/design/frontend/Avanti/innovation/Magento_Theme/web/js/slick-slider-marcas.js
+++ src/app/design/frontend/Avanti/innovation/Magento_Theme/web/js/slick-slider-marcas.js
@@ -7,12 +7,25 @@ define([
     $(document).ready(function () {
         var $slider = $('.slider-marcas-home .pagebuilder-column-line');
 
-            $slider.slick({
-                infinite: false,
-                slidesToShow: 6,
-                slidesToScroll: 6,
-                dots: true,
-                arrows: true
-            });
+        function handleSlider() {
+            if ($(window).width() >= 768) {
+                if (!$slider.hasClass('slick-initialized')) {
+                    $slider.slick({
+                        infinite: false,
+                        slidesToShow: 6,
+                        slidesToScroll: 6,
+                        dots: true,
+                        arrows: true
+                    });
+                }
+            } else {
+                if ($slider.hasClass('slick-initialized')) {
+                    $slider.slick('unslick');
+                }
+            }
+        }
+
+        handleSlider();
+        $(window).on('resize', handleSlider);
     });
 });
