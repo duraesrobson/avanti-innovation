50f8a8f149787d8b8bb740d858cd050f136e5653
Robson Durães <duraesrobson@gmail.com>
2026-01-23 10:47:54 -0300

    feat(checkout): Implementa novo header e footer customizados
    *   Adiciona navegação por etapas customizada (carrinho, entrega, pagamento) no header (desktop) e no corpo (mobile).
    *   Inclui flag de "Compra 100% segura" no header do checkout.
    *   Cria um rodapé customizado com bloco CMS e logos de pagamento.
    *   Desenvolve lógica JS para controle reativo das etapas de checkout.
    *   Aplica estilos específicos para os novos elementos do cabeçalho e remove a newsletter padrão.

---
+++ src/app/design/frontend/Avanti/innovation/Magento_Checkout/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*': {
+            customCheckoutSteps: 'Magento_Checkout/js/view/custom-steps'
+        }
+    },
+    deps: [
+        'customCheckoutSteps'
+    ]
+};
---
+++ src/app/design/frontend/Avanti/innovation/Magento_Checkout/web/js/view/custom-steps.js
@@ -0,0 +1,61 @@
+define([
+    'uiComponent',
+    'Magento_Checkout/js/model/step-navigator',
+    'ko'
+], function (Component, stepNavigator, ko) {
+    'use strict';
+
+    return Component.extend({
+        initialize: function () {
+            this._super();
+
+            this.isCartActive = ko.observable(false);
+            this.isShippingActive = ko.observable(false);
+            this.isPaymentActive = ko.observable(false);
+
+            // CART (fora do checkout SPA)
+            if (window.location.pathname.indexOf('/checkout/cart') !== -1) {
+                this.isCartActive(true);
+                return this;
+            }
+
+            // KO REATIVO (ESSE É O PULO DO GATO)
+            ko.computed(function () {
+                const index = stepNavigator.getActiveItemIndex();
+
+                this.isCartActive(false);
+                this.isShippingActive(false);
+                this.isPaymentActive(false);
+
+                if (index === 0) {
+                    this.isShippingActive(true);
+                }
+
+                if (index === 1) {
+                    this.isPaymentActive(true);
+                }
+            }, this);
+
+            return this;
+        },
+
+        /* ========= AÇÕES DE CLICK ========= */
+
+        goToCart: function () {
+            window.location.href = '/checkout/cart';
+        },
+
+        // uso de isProcessed para evitar erro de fluxo
+        goToShipping: function () {
+            if (stepNavigator.isProcessed('shipping')) {
+                stepNavigator.navigateTo('shipping');
+            }
+        },
+
+        goToPayment: function () {
+            if (stepNavigator.isProcessed('payment')) {
+                stepNavigator.navigateTo('payment');
+            }
+        }
+    });
+});
